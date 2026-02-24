119525a9d0349b85feb80440e70860d18cc5ad2d
haolinyu <yuhaolin@u.boisestate.edu>
2026-02-24 01:11:58 -0700

    week6

--- full_adder.v
+++
@@ -1,9 +0,0 @@
-// Implement module called full_adder
-module full_adder(
-    input a, b, c,
-    output y, count
-
-);
-    assign y = (a & !b & !c ) | (!a & !b & c ) | (a & b & c) | (!a & b & !c );  
-    assign count = (a & c) | ( b & c) | ( a & b);  
-endmodule
